61fef9b567db6f230c3efbf38e59c9fd117045e5
julianagarzond <juliana.garzon-d@mail.escuelaing.edu.co>
2020-01-30 10:01:00 -0500

    Part1

---
+++ IMMORTALS/src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.Queue;
+
+/**
+ * @author hcadavid
+ */
+public class Consumer extends Thread {
+
+    private Queue<Integer> queue;
+
+
+    public Consumer(Queue<Integer> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+            while (true) {
+
+                synchronized (queue) {
+                    try {
+
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (queue.size() > 0) {
+                        int elem = queue.poll();
+                        System.out.println("Consumer consumes " + elem);
+                    }
+                }
+
+
+            }
+
+    }
+}
